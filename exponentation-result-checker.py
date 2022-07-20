a47b7826c19dd62132f98130ee5a6f0917b52ff5
anniejanuary <109144163+anniejanuary@users.noreply.github.com>
2022-07-20 12:06:21 +0200

    Create exponentation-result-checker.py

---
+++ exponentation-result-checker.py
@@ -0,0 +1,18 @@
+#podaj podstawę i wykładnik potęgi, z opcją np 5**0=1, podaj wynik i sprawdz czy jest dobry
+
+#base of power - podstawa potęgi
+print("Enter the base of power: ")
+base=int(input())
+
+#exponent - wykładnik potęgi
+print("Enter the exponent: ")
+exponent=int(input())
+
+print("Enter the exponentation result: ")
+result_input=int(input())
+
+if result_input == base**exponent:
+    print("Result correct")
+else:
+    print("Result incorrect")
+3
